7b7445b74b73b8d725ca5a4e47dc49912b287bf5
Lau Skeeter <lawr@lauragonzalez.cc>
2019-12-01 17:34:00 +0000

    adsdf

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+require("dotenv").config();
+const localtunnel = require("localtunnel");
+const postEndpoint = require("./save.js");
+
+const main = async () => {
+  const port = parseInt(process.argv.pop()) || 80;
+  console.log("Handshake... " + port);
+  const { url } = await localtunnel({ port });
+  console.log("Tunnel set up, waiting for aws");
+  console.log(url);
+  await postEndpoint({ url, port });
+  console.log("done, dont close me!!");
+};
+main();
